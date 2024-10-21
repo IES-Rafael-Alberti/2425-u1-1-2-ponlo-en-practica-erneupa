7d1b73d21056ada0152bf8dab81006f106b435b2
erneupa <aneupaverm@iesrafaelalberti.es>
2024-10-21 22:59:10 +0200

    Add files via upload

---
+++ src/Java.java
@@ -0,0 +1,7 @@
+public class prueba_java {
+    public static void main(String[] args) {
+        System.out.print("Escribe tu nombre: ");
+        String nombre = System.console().readLine();
+        System.out.print("Hola!! " + nombre + ". Este programa está hecho en el lenguaje de programación Java.");
+    }
+}
