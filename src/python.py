95dd282fab524e5c58a228ff52be25237a7afdde
erneupa <aneupaverm@iesrafaelalberti.es>
2024-10-21 22:54:23 +0200

    python

---
+++ src/python.py
@@ -0,0 +1,8 @@
+#Entrada
+nombre = input('Escribe tu nombre: ')
+
+#Procesamiento
+terminado = 'Hola!! ' + nombre + '. Este programa está hecho en el lenguaje de programación: Python'
+
+#Salida
+print(terminado)
